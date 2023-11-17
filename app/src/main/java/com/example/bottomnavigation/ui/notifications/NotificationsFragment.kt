f636780e06dada2c5b4291feb6a944a0be80eac4
Authenticode <developers.authenticode@gmail.com>
2023-11-17 16:33:02 +0530

    Initial commit

---
+++ app/src/main/java/com/example/bottomnavigation/ui/notifications/NotificationsFragment.kt
@@ -0,0 +1,26 @@
+package com.example.bottomnavigation.ui.notifications
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModelProvider
+import com.example.bottomnavigation.R
+import com.example.bottomnavigation.databinding.FragmentNotificationsBinding
+
+class NotificationsFragment : Fragment() {
+
+
+
+    override fun onCreateView(
+            inflater: LayoutInflater,
+            container: ViewGroup?,
+            savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
+    }
+
+}
